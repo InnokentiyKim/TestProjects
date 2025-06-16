a86190d4dc3595e77db90ae10ad1ace6763fee4c
inncent <innokentiykim90@gmail.com>
2025-06-16 17:11:58 +0500

    ToDo models with base functions added

---
+++ ToDoListFastAPI/app/models.py
@@ -0,0 +1,38 @@
+from .config import PG_DSN
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+from sqlalchemy.orm import DeclarativeBase, mapped_column, Mapped
+from sqlalchemy import String, Boolean, DateTime, Integer, func
+import datetime
+
+
+engine = create_async_engine(PG_DSN)
+
+Session = async_sessionmaker(bind=engine, expire_on_commit=False)
+
+class Base(AsyncAttrs, DeclarativeBase):
+
+    @property
+    def id_dict(self):
+        return {"id": self.id}
+
+
+
+class ToDo(Base):
+    __tablename__ = "todo"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String, nullable=False, index=True)
+    description: Mapped[str] = mapped_column(String, nullable=False)
+    important: Mapped[bool] = mapped_column(Boolean, default=False)
+    done: Mapped[bool] = mapped_column(Boolean, default=False)
+    start_time: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    end_time: Mapped[datetime.datetime] = mapped_column(DateTime, nullable=True)
+
+
+async def init_orm():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def close_orm():
+    await engine.dispose()
