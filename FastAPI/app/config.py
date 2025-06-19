b68736acf916d75908d308446c6b211f39bd29cf
inncent <innokentiykim90@gmail.com>
2025-06-19 23:58:47 +0500

    fastapi config and database changed

--- FastAPI/app/config.py
+++ FastAPI/app/config.py
@@ -1,3 +1,4 @@
+from pydantic.v1 import root_validator
 from pydantic_settings import BaseSettings
 
 
@@ -7,6 +8,12 @@ class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
     DB_NAME: str
+
+    @root_validator
+    def get_database_url(cls, val):
+        val['DATABASE_URL'] = (f"postgresql+asyncpg://{val['DB_USER']}:{val['DB_PASSWORD']}"
+                               f"@{val['DB_HOST']}:{val['DB_PORT']}/{val['DB_NAME']}")
+        return val
 
     class Config:
         env_file = ".env"
