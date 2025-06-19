cf18eb7ace93bea9fe1b1bb97d57befe32da11be
inncent <innokentiykim90@gmail.com>
2025-06-19 23:36:10 +0500

    fastapi models added

--- FastAPI/app/hotels/models.py
+++ FastAPI/app/hotels/models.py
@@ -1,10 +1,12 @@
-from sqlalchemy import JSON, Column, Integer, String
+
+from sqlalchemy import Column, Integer, String, JSON
+
 from app.database import Base
 
 
 class Hotels(Base):
-    __tablename__ = "hotels"
-    
+    __tablename__ = 'hotels'
+
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     location = Column(String, nullable=False)
