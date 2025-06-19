3d790098a169a84e85ceeeb232b8758270d9ff9d
inncent <innokentiykim90@gmail.com>
2025-06-19 23:35:42 +0500

    fastapi initial commit

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -1,38 +1,38 @@
 from fastapi import FastAPI, Query
-import uvicorn
 from typing import Optional
 from datetime import date
 from pydantic import BaseModel
-from app.bookings.router import router as router_bookings
-from app.users.router import router as router_users
-
 
 app = FastAPI()
 
-app.include_router(router_users)
-app.include_router(router_bookings)
+
+class HotelResponseSchema(BaseModel):
+    address: str
+    name: str
+    stars: int
 
 @app.get("/hotels")
 def get_hotels(
-    location: str,
-    date_from: date,
-    date_to: date,
-    stars: Optional[int] = Query(None, ge=1, le=5),
-    has_spa: Optional[bool] = None
-):
-    return date_from, date_to
+        location: str,
+        date_from: date,
+        date_to: date,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+        has_spa: Optional[bool] = None,
+) -> list[HotelResponseSchema]:
+    hotels = [
+        {
+            "address": "Gagarin st. 1, Altai",
+            "name": "Super hotel",
+            "stars": 5
+        },
+    ]
+    return hotels
 
-
-class BookingSchema(BaseModel):
+class BookingPostSchema(BaseModel):
     room_id: int
     date_from: date
     date_to: date
-    
 
 @app.post("/bookings")
-def add_booking(booking: BookingSchema):
+def add_booking(booking: BookingPostSchema):
     pass
-
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.0", )
