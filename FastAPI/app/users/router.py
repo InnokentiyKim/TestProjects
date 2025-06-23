3bce58203e03005f14d750cc2d1f2d4bb1ab2386
inncent <innokentiykim90@gmail.com>
2025-06-23 23:52:52 +0500

    fastapi user logout and me views

--- FastAPI/app/users/router.py
+++ FastAPI/app/users/router.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, HTTPException, status, Response
+from fastapi.params import Depends
+
 from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UserDAO
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 from app.users.schemas import UserAuthSchema
 
 
@@ -23,9 +27,19 @@ async def login_user(response: Response, user_data: UserAuthSchema):
     user = await authenticate_user(user_data.email, user_data.password)
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
-    access_token = create_access_token({"sub": user.id})
+    access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True, secure=True)
     return access_token
+
+
+@router.post("/logout")
+async def login_user(response: Response):
+    response.delete_cookie("booking_access_token")
+    return {"detail": "Logged out successfully"}
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
 
 
 
