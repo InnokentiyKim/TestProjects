98a33ecaa27de36da6291f0edb0e2801a34c3fb1
inncent <innokentiykim90@gmail.com>
2025-06-25 20:39:32 +0500

    fastapi add_booking dao and router updated

--- FastAPI/app/bookings/dao.py
+++ FastAPI/app/bookings/dao.py
@@ -1,5 +1,4 @@
 from datetime import date
-
 from app.dao.base import BaseDAO
 from sqlalchemy import select, insert, and_, or_, func
 
@@ -36,14 +35,31 @@ class BookingDAO(BaseDAO):
                 )
             ).cte("booked_rooms")
 
-            rooms_left = select(
+            get_rooms_left = select(
                 (Rooms.quantity - func.count(booked_rooms.c.room_id)).label("rooms_left")
             ).select_from(Rooms).join(
-                booked_rooms, booked_rooms.c.room_id == Rooms.id
+                booked_rooms, booked_rooms.c.room_id == Rooms.id, isouter=True
             ).where(Rooms.id == 1).group_by(
                 Rooms.quantity, booked_rooms.c.room_id
             )
 
-            rooms_left = await session.execute(rooms_left)
-            print(rooms_left.scalar())
+            rooms_left = await session.execute(get_rooms_left)
+            rooms_left = rooms_left.scalar()
 
+            if rooms_left > 0:
+                get_price = select(Rooms.price).filter_by(id=room_id)
+                price = await session.execute(get_price)
+                price = price.scalar()
+                add_booking = insert(Bookings).values(
+                    room_id=room_id,
+                    user_id=user_id,
+                    date_from=date_from,
+                    date_to=date_to,
+                    price=price
+                ).returning(Bookings)
+
+                new_booking = await session.execute(add_booking)
+                await session.commit()
+                return new_booking.scalar()
+            else:
+                return None
--- FastAPI/app/bookings/router.py
+++ FastAPI/app/bookings/router.py
@@ -3,6 +3,7 @@ from datetime import date
 from fastapi import APIRouter, Depends
 from app.bookings.dao import BookingDAO
 from app.bookings.schemas import BookingSchema
+from app.exceptions import RoomCannotBeBookedException
 from app.users.dependencies import get_current_user
 from app.users.models import Users
 
@@ -22,5 +23,7 @@ async def add_booking(
     room_id: int, date_from: date, date_to: date,
     user: Users = Depends(get_current_user)
 ):
-    await BookingDAO.add(user.id, room_id, date_from, date_to)
-
+    booking = await BookingDAO.add(user.id, room_id, date_from, date_to)
+    if not booking:
+        raise RoomCannotBeBookedException
+    return booking
