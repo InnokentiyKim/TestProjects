11c5f0393b2ad7ee9d922233f943fdf50254fd5c
inncent <innokentiykim90@gmail.com>
2025-06-25 20:40:06 +0500

    fastapi room_cannot_be_booked_exception

--- FastAPI/app/exceptions.py
+++ FastAPI/app/exceptions.py
@@ -31,3 +31,7 @@ class IncorrectTokenFormatException(BookingException):
 
 class UserAbsentException(BookingException):
     status_code = status.HTTP_401_UNAUTHORIZED
+
+class RoomCannotBeBookedException(BookingException):
+    status_code = status.HTTP_409_CONFLICT
+    detail = "Не осталось свободных номеров"
