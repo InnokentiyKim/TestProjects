883dbb9187b24711a33d7b5fd1758e107e57f804
inncent <innokentiykim90@gmail.com>
2025-06-27 22:43:02 +0500

    Todo config admin creds

--- ToDoListFastAPI/app/config.py
+++ ToDoListFastAPI/app/config.py
@@ -8,3 +8,8 @@ POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
 
 PG_DSN = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+TOKEN_TTL_SEC = int(os.getenv("TOKEN_TTL_SEC", 24 * 60 * 60))
+
+ADMIN_NAME = os.getenv("ADMIN_NAME", "admin")
+ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD", "1234")
