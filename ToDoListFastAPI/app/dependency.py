3a11968e4f73af9f7d8eab2289d1f29aad797a8b
inncent <innokentiykim90@gmail.com>
2025-06-27 22:44:08 +0500

    Todo dependency token_dependency

--- ToDoListFastAPI/app/dependency.py
+++ ToDoListFastAPI/app/dependency.py
@@ -1,7 +1,12 @@
-from models import Session
+import uuid
+
+from models import Session, Token
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, Header, HTTPException, status
+from sqlalchemy import select
+from config import TOKEN_TTL_SEC
+from datetime import datetime, timedelta
 
 
 async def get_session() -> AsyncSession:
@@ -9,6 +14,21 @@ async def get_session() -> AsyncSession:
         yield session
 
 
-SessionDependency = Annotated[AsyncSession, Depends(get_session)]
+SessionDependency = Annotated[AsyncSession, Depends(get_session, use_cache=True)]
+
+
+async def get_token(x_token: Annotated[uuid.UUID, Header()], session: SessionDependency):
+    token_query = select(Token).where(
+        Token.token == x_token,
+        Token.created_at >= datetime.now() - timedelta(seconds=TOKEN_TTL_SEC)
+    )
+    token = await session.scalar(token_query)
+    if token is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    return token
+
+
+TokenDependency = Annotated[Token, Depends(get_token)]
+
 
 
